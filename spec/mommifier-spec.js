4ef4e2a69e00a1aa88cb4359775a78e4158d788c
Ni WANG <ni.wang@rea-group.com>
2014-11-05 21:56:49 +0800

    return mommy for vowels

--- spec/mommifier-spec.js
+++ spec/mommifier-spec.js
@@ -14,4 +14,15 @@ describe('Mommifier',function(){
 
     expect(result).toBe(str);
   });
+
+  it('should return vowel when given one vowel', function(){
+    var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+
+    var expectedResult = 'mommy';
+
+    vowels.forEach(function(vowel){
+      var result = mommify(vowel);
+      expect(result).toBe(expectedResult);
+    });
+  });
 });
--- src/mommifier.js
+++ src/mommifier.js
@@ -1,3 +1,4 @@
 function mommify(str){
-  return str;
+  var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  return vowels.indexOf(str) >= 0 ? 'mommy' : str;
 }
